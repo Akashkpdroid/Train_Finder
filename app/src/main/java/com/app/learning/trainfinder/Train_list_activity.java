5058c98b8d3bd1c8cbcd0b493260d935ab3def20
Biswadip-24 <biswadipmandal2001@gmail.com>
2020-06-27 19:52:50 +0530

    Update

--- app/src/main/java/com/app/learning/trainfinder/Train_list_activity.java
+++ app/src/main/java/com/app/learning/trainfinder/Train_list_activity.java
@@ -34,6 +34,8 @@ public class Train_list_activity extends AppCompatActivity {
         ArrayList<Row_item> RowList=new ArrayList<>();
         RowList.add(new Row_item("Train Number: 02303","Train Name: Poorva Express","Arrival Time: 06:05","Departure Time: 07:45","Travel Time: 22:20H"));
         RowList.add(new Row_item("Train Number: 02381","Train Name: Poorva Express","Arrival Time: 06:05","Departure Time: 08:00","Travel Time: 22:05H"));
+        RowList.add(new Row_item("Train Number: 02385","Train Name: Chennai Express","Arrival Time: 16:05","Departure Time: 08:00","Travel Time: 32:05H"));
+        RowList.add(new Row_item("Train Number: 02395","Train Name: Mumbai Mail","Arrival Time: 16:45","Departure Time: 08:00","Travel Time: 31:25H"));
         RowList.add(new Row_item("Train Number: 02301","Train Name: HWH NDLS AC SPL","Arrival Time: 10:00","Departure Time: 16:45","Travel Time: 17:15H"));
 
         mRecyclerView=findViewById(R.id.RecyclerView);
